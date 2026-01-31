ab3a00102544172546815172177fedf3aed3c7da
Oswaaaald <maxim.shitty@proton.me>
2026-02-01 00:48:12 +0100

    fix: add 'x-csrf-token' to allowed headers for CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -281,7 +281,7 @@ async function registerPlugins() {
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'],
     exposedHeaders: ['Content-Range', 'X-Content-Range'],
   });
 
